58182da526c7b3b19d675b460810dce9320d1c78
HopSwap <79823005+hopswap@users.noreply.github.com>
2021-09-14 16:10:57 +0100

    Update index.ts

--- pancake-swap-interface-v1/src/constants/multicall/index.ts
+++ pancake-swap-interface-v1/src/constants/multicall/index.ts
@@ -2,7 +2,7 @@ import { ChainId } from '@pancakeswap-libs/sdk'
 import MULTICALL_ABI from './abi.json'
 
 const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: '0x5cf89F6159CF12735FbB3AF61EB7290AD8e7563A', // TODO
+  [ChainId.MAINNET]: '0x323d757f1B7cE07ba967be1238e7F9f2f153fD0E', // TODO
   [ChainId.BSCTESTNET]: '0x5cf89F6159CF12735FbB3AF61EB7290AD8e7563A'
 }
 
